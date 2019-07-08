c7ccc9c16f7e0523fd0adbf1ac2cb4ec35023118
hanchang.liu@capitalonline.net <hanchang.liu@capitalonline.net>
2019-07-08 08:02:02 +0800

    SimpleDateFormat

---
+++ src/main/java/com/mmall/concurrency/example/commonUnsafe/DateFormatExample2.java
@@ -0,0 +1,52 @@
+package com.mmall.concurrency.example.commonUnsafe;
+
+import com.mmall.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@NotThreadSafe
+public class DateFormatExample2 {
+    //SimpleDateFormat 是线程不安全的
+    private static SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyyMMdd");
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i=0;i<clientTotal;i++){
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e){
+                    log.error("exception",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+
+    }
+
+    private static void add() {
+        try {
+            //SimpleDateFormat 是线程不安全的，使用堆栈封闭的方法解决线程不安全的问题
+            SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyyMMdd");
+            simpleDateFormat.parse("20190708");
+        }catch (Exception e){
+            log.error("parse exception",e);
+        }
+    }
+}
